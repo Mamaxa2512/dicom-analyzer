d15ec233662170aba5639a2f9b93df2b5e3f35a7
Vladyslav Danyliuk <vladyslav.danyliuk@student.tuke.sk>
2026-06-23 21:14:24 +0200

    parser is ready

--- app/dicom_parser.py
+++ app/dicom_parser.py
@@ -1,23 +1,34 @@
 """
 Module for parsing DICOM files using pydicom.
 """
+# pyrefly: ignore [missing-import]
 import pydicom
+# pyrefly: ignore [missing-import]
+import numpy as np
+from PIL import Image
+
+
 
 
 def parse_dicom(filepath: str) -> dict:
     dataset = pydicom.dcmread(filepath)
-    return {'name': str(dataset.PatientName), 'age': dataset.PatientAge, 'sex': dataset.PatientSex, 'study_date': dataset.StudyDate, 'study_description': dataset.StudyDescription}
+    return {'name': str(dataset.PatientName), 'age': dataset.PatientAge, 'sex': dataset.PatientSex, 'study_date': dataset.StudyDate, 'study_description': dataset.StudyDescription, 'modality': dataset.Modality}
 
 def get_pixel_array(filepath: str):
-    """
-    Extract pixel data from DICOM.
-    TODO: Phase 1 - Implement
-    """
-    pass
+    arr = pydicom.dcmread(filepath).pixel_array
+    return arr
+    
 
 def get_image_as_png(filepath: str):
-    """
-    Convert DICOM pixel data to PNG for web display.
-    TODO: Phase 1 - Implement
-    """
-    pass
+    arr = get_pixel_array(filepath)
+    min = np.min(arr)
+    max = np.max(arr)
+    
+    norm_arr = (arr - min) / (max - min)*255
+
+    ready_arr = norm_arr.astype(np.uint8)
+
+    img = Image.fromarray(ready_arr)
+    return img
+    
+    
